5a842c4bb1cc6c8f5a8fd5296d9d576487c83026
Nick-M1 <nicholasmalloura@gmail.com>
2023-02-11 21:30:35 +0000

    V1

---
+++ app/(user_mainlayout)/test/page.tsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+// import StoreInitialiser from "../../../components/StoreInitialiser.tsx.txt";
+
+export default function Home() {
+    return (
+        <div>
+            {/*<StoreInitialiser name={'james'} cart={[]} cartTotal={5}/>*/}
+            <div className='py-20 text-red-500'>
+                Home Page.
+            </div>
+        </div>
+        
+    )
+}

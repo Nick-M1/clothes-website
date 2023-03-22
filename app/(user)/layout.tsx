0fdf4466648218a226dcf233c9ede14b4eb32f6a
Nick-M1 <nicholasmalloura@gmail.com>
2023-03-22 14:01:43 +0000

    updated header

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -12,17 +12,18 @@ export default async function Layout({children}: { children: React.ReactNode }) 
     const sessionAuth = await getServerSession(authOptions)
 
     return (
-        <div>
-            <div className="z-10 absolute w-full"><Banner sessionAuth={sessionAuth}/></div>
-            <div className="z-20 absolute w-full"><TempBanner/></div>
-            <div className="z-20"><AddItemToBasket/></div>
+        <>
+            <SideOver/>
+            <div>
+                <div className="z-10 absolute w-full"><Banner sessionAuth={sessionAuth}/></div>
+                <div className="z-20 absolute w-full"><TempBanner/></div>
+                <div className="z-20"><AddItemToBasket/></div>
+                <div className="-z-20 pt-20">
+                    {children}
+                </div>
 
-            <div className="z-30"><SideOver/></div>
-            <div className="-z-20 pt-20">
-                {children}
+                <Footer/>
             </div>
-
-            <Footer/>
-        </div>
+        </>
     )
 }

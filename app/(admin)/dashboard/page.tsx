0ec3d5f7c6b12afbcd4bb8aff9337b5ccfd43884
Nick-M1 <nicholasmalloura@gmail.com>
2023-04-17 13:07:19 +0100

    updated packages

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,17 +1,8 @@
-import React from 'react';
-import AddAllToStripe from "../../../components/stripe/AddAllToStripe";
-import ResetCart from "../../../components/checkout/ResetCart";
-import Demo1 from "../../../components/redis/Demo1";
-// import AddAllToFirebase from "../../../components/firebase/AddAllToFirebase.tsx.txt";
-
 
 export default function Page() {
     return (
         <div>
-            {/*<ResetCart/>*/}
-            <Demo1/>
-            {/*<div className='m-10'><AddAllToStripe/></div>*/}
-            {/*<div className='m-10'><AddAllToFirebase/></div>*/}
+            Dashboard
         </div>
     );
 }
